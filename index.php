7a4a3a4629f0f7eaff4a4c0fe2d5bce34e642dcc
Felps7k <felipefarias97@icloud.com>
2022-09-22 00:19:09 -0300

    :tada: Commit com estrutura inicial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Links -->
+    <link rel="icon" href="/favicons/favicon.ico">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Projeto IP</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <ul>
+                    <li><a href="#index">Início</a></li>
+                    <li><a href="whois.php">Whois</a></li>
+                    <li><a href="geoip.php">Geo IP</a></li>
+                    <li><a href="yourip.php">Seu IP</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <div>
+        <a name="index">
+            <h1> Projeto IP</h1>
+        </a>
+        <p>Corpo inicial.</p>
+    </div>
+</body>
+</html>
