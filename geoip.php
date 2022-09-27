88b7edd939148dce1000f9808a7e92044c14214d
Felps7k <felipefarias97@icloud.com>
2022-09-26 23:15:32 -0300

    :construction_worker: Adicionado script que faz a busca GeoIP

--- geoip.php
+++ geoip.php
@@ -1,3 +1,30 @@
-<p>
-    GeoIP
-</p>
+<div class="field">
+    <form action="" method="POST">
+        <input class="" name="ip" type="text" placeholder="Informe o endereço IP" v-model="geoip_ip">
+        <input class="" name="send" type="submit" value="Buscar">
+    </form>
+</div>
+<div>
+    <?php
+        $ip = $_POST['ip'];
+        if($ip){
+            $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
+            if($query && $query['status'] == 'success')
+            {
+                echo 'IP: ' . $ip;
+                echo '<br />';
+                
+                echo 'Provedor: ' . $query['isp'];
+                echo '<br />';
+                
+                echo 'Cidade: ' . $query['city'];
+                echo '<br />';
+                
+                echo 'País: ' . $query['country'];
+                echo '<br />';
+                
+                echo 'Região: ' . $query['regionName'];
+            }    
+        }
+    ?>
+</div>
