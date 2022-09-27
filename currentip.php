d4449a2df35827726038f4a7cdef2255e9d6f4e5
Felps7k <felipefarias97@icloud.com>
2022-09-26 23:35:50 -0300

    :construction_worker: Adicionado script que mostra seu IP atual

--- currentip.php
+++ currentip.php
@@ -1,3 +1,22 @@
-<p>
-    CurrentIP
-</p>
+<?php
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if($ip){
+        $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
+        if($query && $query['status'] == 'success')
+        {
+            echo 'IP: ' . $ip;
+            echo '<br />';
+            
+            echo 'Provedor: ' . $query['isp'];
+            echo '<br />';
+
+            echo 'Cidade: ' . $query['city'];
+            echo '<br />';
+            
+            echo 'País: ' . $query['country'];
+            echo '<br />';
+            
+            echo 'Região: ' . $query['regionName'];
+        }    
+    }
+?>
